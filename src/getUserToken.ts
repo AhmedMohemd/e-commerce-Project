0484b6e65be32a62943a82bec287d897c4f582ed
AhmedMohamed <ahmed884822277@gmail.com>
2025-09-19 05:14:27 +0300

    Update Token session

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,8 +3,13 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
+  const TokenSession =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
   const cookiesData = await cookies();
-  const userToken = cookiesData.get("next-auth.session-token")?.value;
+
+  const userToken = cookiesData.get(TokenSession)?.value;
   const data = await decode({
     token: userToken,
     secret: process.env.NEXTAUTH_SECRET!,
